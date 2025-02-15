976d549ba42db413c31076ab67c9fb0dbafda881
CatOfJupit3r <catofjupit3r@gmail.com>
2025-02-15 15:32:34 +0200

    fix(env): manually load `.env` file based on NODE_ENV

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,4 +1,31 @@
+import { config } from 'dotenv';
+import fs from 'fs';
 import { z } from 'zod';
+
+switch (process.env.NODE_ENV) {
+    case 'test':
+    case 'production': {
+        if (fs.existsSync(`.${process.env.NODE_ENV}.env`)) {
+            console.log(`Using .${process.env.NODE_ENV}.env file`);
+            config({ path: `bin/config/.${process.env.NODE_ENV}.env` });
+            console.log('Loaded .production.env file');
+        } else {
+            console.log(
+                `File .${process.env.NODE_ENV}.env does not exist. Make sure you passed environment variables!`
+            );
+        }
+        // else, we assume they are passed to by container
+        // e.g. Heroku, Docker, etc.
+        break;
+    }
+    case 'development':
+        console.log('Using .development.env file');
+        config({ path: 'bin/config/.development.env' });
+        console.log('Loaded .development.env file');
+        break;
+    default:
+        throw new Error(`Invalid NODE_ENV: ${process.env.NODE_ENV}.`);
+}
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
